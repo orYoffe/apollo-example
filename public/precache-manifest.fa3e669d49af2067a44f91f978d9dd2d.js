ec82923be54902114f28ec0d46775b55fe691f13
orYoffe <ragiop07@gmail.com>
2019-03-14 15:04:58 +0100

    added static files

---
+++ public/precache-manifest.fa3e669d49af2067a44f91f978d9dd2d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "929409badf2bf774ffb9",
+    "url": "/static/js/main.00560238.chunk.js"
+  },
+  {
+    "revision": "e96151d8699a7a64c091",
+    "url": "/static/js/2.6b7a5746.chunk.js"
+  },
+  {
+    "revision": "3c9b841317acb1c30df648d8942b9a91",
+    "url": "/index.html"
+  }
+];
